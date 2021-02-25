6fbb49fd2913441b267b78a576985e5cde539483
uallrightm8 <79641717+uallrightm8@users.noreply.github.com>
2021-02-25 16:24:06 +0000

    Update rgbToOmnis.sh
    Ensure each appended byte it exactly not smaller than 8 bits

--- rgbToOmnis.sh
+++ rgbToOmnis.sh
@@ -5,10 +5,9 @@ declare -x omnisColor
 toBinary(){
     n="$1"
     bit=$2
-    while [ "$n" -gt 0 ]; do
-        bit="$(( n&1 ))$bit";
-        : $(( n >>= 1 ))
-    done
+    while [ "$n" -gt 0 ]; do bit="$(( n&1 ))$bit"; : $(( n >>= 1 )); done
+    missingBits=$(( 8-${#bit} ))
+    if [ "$missingBits" -gt 0 ]; then for i in $(seq 1 $missingBits); do bit=0$bit; done fi
     omnisColor=$omnisColor$bit
 }
 
